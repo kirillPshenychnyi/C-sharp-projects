cdd1a261ab1fb35cf72ca5e022b7938bd7add35a
Kirill Pshenichnyi <pshenychnyi96@gmail.com>
2016-07-03 13:21:56 +0300

    Created account hierarchy

---
+++ bank/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    class Account
+    {
+
+/***************************************************************************/
+
+        protected Account( string _fullName, int _id, double _currentBalance )
+        {
+            m_fullName = _fullName;
+
+            m_id = _id;
+
+            m_currentBalance = _currentBalance;
+        }
+
+        public virtual int countOverdraftLimit()
+        {
+            return 0;
+        }
+
+/***************************************************************************/
+
+        public string fullName { get { return m_fullName; }  }
+
+        public int ID { get { return m_id; }  }
+
+        public double Balance { get { return m_currentBalance;  }
+                                set { m_currentBalance = value; }
+        }
+
+/***************************************************************************/
+
+        private string m_fullName;
+
+        private int m_id;
+
+        private double m_currentBalance;
+
+/***************************************************************************/
+
+    } // class Account
+}
---
+++ bank/OverdraftAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    class OverdraftAccount : Account
+    {
+
+/***************************************************************************/
+
+        public OverdraftAccount( string _fullName, int _id, double _currentBalance, int _overdraftLimit ) 
+            :   base( _fullName, _id, _currentBalance)
+        {
+            m_overdraftLimit = _overdraftLimit;
+        }
+
+        public override int countOverdraftLimit()
+        {
+            return m_overdraftLimit;
+        }
+
+/***************************************************************************/
+
+        public int OverdraftLimit { get { return m_overdraftLimit; } }
+
+/***************************************************************************/
+
+        private int m_overdraftLimit;
+
+/***************************************************************************/
+
+    } // class Account
+}
